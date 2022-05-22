08e905099ba6950ff568af378ac4cf6b2b50f958
Chris Skaaning <cskaaning@digitalocean.com>
2022-05-22 21:26:06 +0100

    Adding images

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react';
+
+function importAll(r) {
+  return r.keys().map(r);
+}
 
 function App() {
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    setImages(importAll(require.context('./images', false, /\.(png|jpe?g|svg)$/)));
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <span className="headline">Arran's Art</span>
       </header>
+      {images.map(image => {
+        return <img src={image} className="photo var3" />;
+      })}
     </div>
   );
 }
